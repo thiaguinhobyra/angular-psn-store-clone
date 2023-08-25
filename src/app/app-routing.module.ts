96f5f4a7dfbb043c8e7e51957d0f6c2a649b8989
thiago.machado <thiago.machado@seati.ma.gov.br>
2023-08-25 17:52:37 -0300

    feat: inclui novas funções

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { 
@@ -7,9 +8,21 @@ const routes: Routes = [
     pathMatch: 'full',
     redirectTo: 'home', 
   },
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
   {
     path: 'home', 
     loadChildren:() => import('./pages/home/home.module').then((m)=>m.HomeModule),
+  },
+  {
+    path: 'novousuario',
+    loadChildren:() => import('./pages/novo-usuario/novo-usuario.module').then((m)=>m.NovoUsuarioModule),
+  },
+  {
+    path: 'jogos',
+    loadChildren:() => import('./jogos/jogos.module').then((m)=>m.JogosModule),
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MenuBarModule } from './components/menu-bar/menu-bar.module';
+import { NovoUsuarioModule } from './pages/novo-usuario/novo-usuario.module';
 
 @NgModule({
   declarations: [
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    MenuBarModule,
+    NovoUsuarioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
