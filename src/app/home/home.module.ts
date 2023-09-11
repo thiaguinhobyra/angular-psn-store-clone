e373147bdb276269ab324da7daa6c5ec9ee89995
thiago.machado <thiago.machado@seati.ma.gov.br>
2023-09-11 09:38:31 -0300

    refactor: limpa codigo

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-import { CardComponent } from 'src/app/components/card/card.component';
-import { CardLabelComponent } from 'src/app/components/card/card-label/card-label.component';
-import { CardPricingComponent } from 'src/app/components/card/card-pricing/card-pricing.component';
-import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MensagemModule } from 'src/app/components/mensagem/mensagem.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 @NgModule({
